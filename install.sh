49b1ac867d1678bfe13cdd69bf229dd93e6f0846
jackgerrits <jackgerrits95@gmail.com>
2016-05-30 17:22:02 +0930

    initial commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# vim
+ln -sv ${BASEDIR}/vimrc ~/.vimrc
+
+# zsh
+ln -sv ${BASEDIR}/zshrc ~/.zshrc
+
+# git
+ln -sv ${BASEDIR}/gitconfig ~/.gitconfig
+
+
+if [ $(uname) == "Darwin"  ]; then
+    # run the osx script
+    echo "running osx setup script"
+    ./osx.sh
+fi
---
+++ osx.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# brew
+which brew > /dev/null
+if [ $? == 1  ]; then
+    # install brew
+    echo "installing brew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew install zsh
+chsh -s $(which zsh)
+
+# ohmyzsh
+if [ ! -d ~/.oh-my-zsh  ]; then
+    # install oh my zsh
+    echo "installing oh my zsh..."
+    curl -L http://install.ohmyz.sh | sh
+fi
+
+# remove accent when holding down a key
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# speed up window maximize time
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+
+# Save screenshots to custom folder (default: Desktop)
+defaults write com.apple.screencapture location -string "~/Pictures/screenshots"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+## Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+echo "note you may have to restart/re-login for all changes to take effect"
